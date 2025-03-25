ae799f2f688bd1d48771981e1043aa455dcb0117
samihazaman <samihaz2009@gmail.com>
2025-03-25 07:00:59 -0400

    create game component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Lobby from "./components/Lobby";
+import Game from "./components/Game";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Lobby />} />
+        <Route path="/game" element={<Game />} />
       </Routes>
     </Router>
   );
